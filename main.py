0b3ed694f46c93dc9afdfe2bbcdf1d568a2fc930
Mitja Pak <pmitja1@gmail.com>
2020-11-23 21:49:40 +0100

    First commit.

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/fakebook")
+def fakebook():
+    return render_template("index_fakebook.html")
+
+@app.route("/hairdresser_hans")
+def hairdresser():
+    return render_template("index_hairdresser_hans.html")
+
+if __name__ == '__main__':
+    app.run()
